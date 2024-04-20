39f6ca8b8005f8f442e92829e270ee10c275e59f
Shadrackssien <shadrack6960@gmail.com>
2024-04-20 23:07:21 +0000

    Workflow Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import FeatureSection from "./components/FeatureSection"
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
+import Workflow from "./components/Workflow"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <HeroSection />
       <FeatureSection />
+      <Workflow />
     </>
   )
 }
