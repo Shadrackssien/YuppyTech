686e442ace952426a4ee0642062aff6d87cdd42c
Shadrackssien <shadrack6960@gmail.com>
2024-04-30 08:41:38 +0000

    Debuging

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -63,37 +63,37 @@ export const features = [
     icon: <BotMessageSquare />,
     text: "Drag-and-Drop Interface",
     description:
-      "Easily design and arrange your VR environments with a user-friendly drag-and-drop interface.",
+      "Easily design and arrange your YT environments with a user-friendly drag-and-drop interface.",
   },
   {
     icon: <Fingerprint />,
     text: "Multi-Platform Compatibility",
     description:
-      "Build VR applications that run seamlessly across multiple platforms, including mobile, desktop, and VR headsets.",
+      "Build YT applications that run seamlessly across multiple platforms, including mobile, desktop, and YT headsets.",
   },
   {
     icon: <ShieldHalf />,
     text: "Built-in Templates",
     description:
-      "Jumpstart your VR projects with a variety of built-in templates for different types of applications and environments.",
+      "Jumpstart your YT projects with a variety of built-in templates for different types of applications and environments.",
   },
   {
     icon: <BatteryCharging />,
     text: "Real-Time Preview",
     description:
-      "Preview your VR application in real-time as you make changes, allowing for quick iterations and adjustments.",
+      "Preview your YT application in real-time as you make changes, allowing for quick iterations and adjustments.",
   },
   {
     icon: <PlugZap />,
     text: "Collaboration Tools",
     description:
-      "Work together with your team in real-time on VR projects, enabling seamless collaboration and idea sharing.",
+      "Work together with your team in real-time on YT projects, enabling seamless collaboration and idea sharing.",
   },
   {
     icon: <GlobeLock />,
     text: "Analytics Dashboard",
     description:
-      "Gain valuable insights into user interactions and behavior within your VR applications with an integrated analytics dashboard.",
+      "Gain valuable insights into user interactions and behavior within your YT applications with an integrated analytics dashboard.",
   },
 ];
 
@@ -101,22 +101,22 @@ export const checklistItems = [
   {
     title: "Code merge made easy",
     description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
+      "Track the performance of your YT apps and gain insights into user behavior.",
   },
   {
     title: "Review code without worry",
     description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
+      "Track the performance of your YT apps and gain insights into user behavior.",
   },
   {
     title: "AI Assistance to reduce time",
     description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
+      "Track the performance of your YT apps and gain insights into user behavior.",
   },
   {
     title: "Share work in minutes",
     description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
+      "Track the performance of your YT apps and gain insights into user behavior.",
   },
 ];
 
